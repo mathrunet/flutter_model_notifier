fc925224dc925d855168903370e6d3baaf0d905c
mathru <user@mathru.net>
2021-04-20 09:50:20 +0900

    インターフェースを追加

--- lib/src/dynamic_collection_model.dart
+++ lib/src/dynamic_collection_model.dart
@@ -2,4 +2,4 @@ part of model_notifier;
 
 /// Collection model for flexibly modifying the contents of an object that is primarily a [Map<String, dynamic>].
 abstract class DynamicCollectionModel<T extends DynamicDocumentModel>
-    implements ListModel<T> {}
+    implements ListModel<T>, List<T> {}
--- lib/src/dynamic_document_model.dart
+++ lib/src/dynamic_document_model.dart
@@ -2,4 +2,4 @@ part of model_notifier;
 
 /// Documentation model for flexibly modifying the contents of an object that is primarily a [Map<String, dynamic>].
 abstract class DynamicDocumentModel
-    implements DocumentModel<Map<String, dynamic>> {}
+    implements DocumentModel<Map<String, dynamic>>, Map<String, dynamic> {}
